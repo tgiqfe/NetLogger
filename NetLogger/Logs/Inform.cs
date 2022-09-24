81cad8d52ac66e4f0ebe48e6fae1dff1b9f45fed
tgiqfe <tqji31001@gmail.com>
2022-09-24 16:25:06 +0900

    check update code.

---
+++ NetLogger/Logs/Inform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLogger.Logs
+{
+    //  ステータス情報出力用
+    internal class Inform<T> : Storing<T>
+    {
+
+
+        public Inform(string logDir, string tableName)
+        {
+
+        }
+
+        public override async Task OutputTextAsync()
+        {
+            using (await _lock.LockAsync())
+            {
+                var items = _collection.FindAll();
+                using (var sw = new StreamWriter("<output dir>", false, Encoding.UTF8))
+                {
+                    foreach(var item in items)
+                    {
+                        sw.WriteLine(item.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NetLogger/Logs/Storing.cs
@@ -0,0 +1,84 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLogger.Logs
+{
+    internal class Storing<T> : IDisposable, IRepeatable
+    {
+        protected static AsyncLock _lock = null;
+
+        public string OutputDir { get; set; }
+        public string OutputFilePath { get; set; }
+        public string OutputDbPath { get; set; }
+        public string TableNAme { get; set; }
+
+        #region Protected parameter
+
+        protected LiteDatabase _liteDB = null;
+        protected ILiteCollection<T> _collection = null;
+        protected DbManager _manager = null;
+        protected LogServer _logServer = null;
+        protected HttpClient _client = null;
+
+        #endregion
+
+
+
+
+        public virtual Task OutputTextAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public virtual Task OutputRemoteAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+
+
+
+        #region Repeatable
+
+        public virtual Task Work()
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+        #region Close method
+
+        public virtual void Close()
+        {
+        }
+
+        #endregion
+        #region Dispose
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Close();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
